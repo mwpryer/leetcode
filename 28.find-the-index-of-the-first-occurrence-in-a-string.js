c60e4e1e5e9a20fcd772115397989e9dffb34122
Matt <mwpryer@gmail.com>
2023-07-04 19:51:00 +0800

    Solve 28.find-the-index-of-the-first-occurrence-in-a-string.js

---
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=28 lang=javascript
+ *
+ * [28] Find the Index of the First Occurrence in a String
+ *
+ * https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+ *
+ * algorithms
+ * Easy (39.72%)
+ * Likes:    3887
+ * Dislikes: 213
+ * Total Accepted:    1.8M
+ * Total Submissions: 4.6M
+ * Testcase Example:  '"sadbutsad"\n"sad"'
+ *
+ * Given two strings needle and haystack, return the index of the first
+ * occurrence of needle in haystack, or -1 if needle is not part of
+ * haystack.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: haystack = "sadbutsad", needle = "sad"
+ * Output: 0
+ * Explanation: "sad" occurs at index 0 and 6.
+ * The first occurrence is at index 0, so we return 0.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: haystack = "leetcode", needle = "leeto"
+ * Output: -1
+ * Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= haystack.length, needle.length <= 10^4
+ * haystack and needle consist of only lowercase English characters.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  for (let i = 0; i <= haystack.length - needle.length; i++) {
+    let match = true
+    for (let j = 0; j < needle.length; j++) {
+      if (haystack[i + j] !== needle[j]) {
+        match = false
+        break
+      }
+    }
+    if (match) return i
+  }
+  return -1
+}
+// @lc code=end
