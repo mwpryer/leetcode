beb02cc23e30dd85e5ca9b9cb3a057c19fde16fb
Matt <mwpryer@gmail.com>
2023-07-05 03:20:46 +0800

    Solve 35.search-insert-position.js

---
+++ 35.search-insert-position.js
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode id=35 lang=javascript
+ *
+ * [35] Search Insert Position
+ *
+ * https://leetcode.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (43.83%)
+ * Likes:    13750
+ * Dislikes: 601
+ * Total Accepted:    2.3M
+ * Total Submissions: 5.2M
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * Given a sorted array of distinct integers and a target value, return the
+ * index if the target is found. If not, return the index where it would be if
+ * it were inserted in order.
+ *
+ * You must write an algorithm with O(log n) runtime complexity.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [1,3,5,6], target = 5
+ * Output: 2
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [1,3,5,6], target = 2
+ * Output: 1
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: nums = [1,3,5,6], target = 7
+ * Output: 4
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= nums.length <= 10^4
+ * -10^4 <= nums[i] <= 10^4
+ * nums contains distinct values sorted in ascending order.
+ * -10^4 <= target <= 10^4
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let left = 0
+  let right = nums.length - 1
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2)
+    if (nums[mid] === target) {
+      return mid
+    } else if (nums[mid] < target) {
+      left = mid + 1
+    } else {
+      right = mid - 1
+    }
+  }
+  return right + 1
+}
+// @lc code=end
