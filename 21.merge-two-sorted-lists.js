8bf5c57b6fac3f8973734aacd215569c6a788e89
Matt <mwpryer@gmail.com>
2023-07-04 18:31:52 +0800

    Solve 21.merge-two-sorted-lists.js

---
+++ 21.merge-two-sorted-lists.js
@@ -0,0 +1,94 @@
+/*
+ * @lc app=leetcode id=21 lang=javascript
+ *
+ * [21] Merge Two Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (62.82%)
+ * Likes:    18907
+ * Dislikes: 1763
+ * Total Accepted:    3.3M
+ * Total Submissions: 5.3M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * You are given the heads of two sorted linked lists list1 and list2.
+ *
+ * Merge the two lists in a one sorted list. The list should be made by
+ * splicing together the nodes of the first two lists.
+ *
+ * Return the head of the merged linked list.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: list1 = [], list2 = []
+ * Output: []
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: list1 = [], list2 = [0]
+ * Output: [0]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in both lists is in the range [0, 50].
+ * -100 <= Node.val <= 100
+ * Both list1 and list2 are sorted in non-decreasing order.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  let curr = new ListNode()
+  const temp = curr
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      curr.next = list1
+      list1 = list1.next
+    } else {
+      curr.next = list2
+      list2 = list2.next
+    }
+    curr = curr.next
+  }
+  while (list1) {
+    curr.next = list1
+    list1 = list1.next
+    curr = curr.next
+  }
+  while (list2) {
+    curr.next = list2
+    list2 = list2.next
+    curr = curr.next
+  }
+  return temp.next
+}
+// @lc code=end
