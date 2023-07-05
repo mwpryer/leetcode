8a535f4d160384f731a5746c65e7a804e365f866
Matt <mwpryer@gmail.com>
2023-07-05 21:36:13 +0800

    Solve 58.length-of-last-word.js

---
+++ 58.length-of-last-word.js
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=58 lang=javascript
+ *
+ * [58] Length of Last Word
+ *
+ * https://leetcode.com/problems/length-of-last-word/description/
+ *
+ * algorithms
+ * Easy (44.31%)
+ * Likes:    3482
+ * Dislikes: 182
+ * Total Accepted:    1.2M
+ * Total Submissions: 2.7M
+ * Testcase Example:  '"Hello World"'
+ *
+ * Given a string s consisting of words and spaces, return the length of the
+ * last word in the string.
+ *
+ * A word is a maximal substring consisting of non-space characters only.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: s = "Hello World"
+ * Output: 5
+ * Explanation: The last word is "World" with length 5.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: s = "   fly me   to   the moon  "
+ * Output: 4
+ * Explanation: The last word is "moon" with length 4.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: s = "luffy is still joyboy"
+ * Output: 6
+ * Explanation: The last word is "joyboy" with length 6.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= s.length <= 10^4
+ * s consists of only English letters and spaces ' '.
+ * There will be at least one word in s.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  let len = 0
+  for (i = s.length - 1; i >= 0; i--) {
+    if (s[i] === " ") {
+      if (len > 0) return len
+    } else {
+      len++
+    }
+  }
+  return len
+}
+// @lc code=end
