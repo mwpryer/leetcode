17d19e644abbeb641bd737f3720aedf5668472c2
Matt <mwpryer@gmail.com>
2023-07-13 22:37:06 +0800

    Solve 110.balanced-binary-tree.js

---
+++ 110.balanced-binary-tree.js
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=110 lang=javascript
+ *
+ * [110] Balanced Binary Tree
+ *
+ * https://leetcode.com/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (49.66%)
+ * Likes:    9329
+ * Dislikes: 529
+ * Total Accepted:    1.2M
+ * Total Submissions: 2.3M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, determine if it is height-balanced.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [1,2,2,3,3,null,null,4,4]
+ * Output: false
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = []
+ * Output: true
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [0, 5000].
+ * -10^4 <= Node.val <= 10^4
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+  if (!root) return true
+  function getHeight(root) {
+    if (!root) return 0
+    const left = getHeight(root.left)
+    const right = getHeight(root.right)
+    return Math.max(left, right) + 1
+  }
+  const left = getHeight(root.left)
+  const right = getHeight(root.right)
+  return Math.abs(left - right) <= 1 && isBalanced(root.left) && isBalanced(root.right)
+}
+// @lc code=end
