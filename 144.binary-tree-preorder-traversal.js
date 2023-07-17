f25f03559a4c24c72f4dfbf7b3d112d97e22e4b0
Matt <mwpryer@gmail.com>
2023-07-18 02:20:39 +0800

    Solve 144.binary-tree-preorder-traversal.js

---
+++ 144.binary-tree-preorder-traversal.js
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=144 lang=javascript
+ *
+ * [144] Binary Tree Preorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Easy (67.44%)
+ * Likes:    7117
+ * Dislikes: 185
+ * Total Accepted:    1.4M
+ * Total Submissions: 2M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given the root of a binary tree, return the preorder traversal of its nodes'
+ * values.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [1,null,2,3]
+ * Output: [1,2,3]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = []
+ * Output: []
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = [1]
+ * Output: [1]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ *
+ *
+ *
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+  if (!root) return []
+  const left = preorderTraversal(root.left)
+  const right = preorderTraversal(root.right)
+  return [root.val, ...left, ...right]
+}
+// @lc code=end
