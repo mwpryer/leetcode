2c638c5f5a486d10d3f79115b11b817f6e81c66e
Matt <mwpryer@gmail.com>
2023-07-21 00:16:27 +0800

    Solve 2.add-two-numbers.js

---
+++ 2.add-two-numbers.js
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode id=2 lang=javascript
+ *
+ * [2] Add Two Numbers
+ *
+ * https://leetcode.com/problems/add-two-numbers/description/
+ *
+ * algorithms
+ * Medium (40.72%)
+ * Likes:    27106
+ * Dislikes: 5218
+ * Total Accepted:    3.7M
+ * Total Submissions: 9.2M
+ * Testcase Example:  '[2,4,3]\n[5,6,4]'
+ *
+ * You are given two non-empty linked lists representing two non-negative
+ * integers. The digits are stored in reverse order, and each of their nodes
+ * contains a single digit. Add the two numbers and return the sum as a linked
+ * list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the
+ * number 0 itself.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: l1 = [0], l2 = [0]
+ * Output: [0]
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * Output: [8,9,9,9,0,0,0,1]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in each linked list is in the range [1, 100].
+ * 0 <= Node.val <= 9
+ * It is guaranteed that the list represents a number that does not have
+ * leading zeros.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  let curr = new ListNode()
+  const temp = curr
+  let carry = 0
+  while (l1 || l2 || carry) {
+    let sum = carry
+    if (l1) {
+      sum += l1.val
+      l1 = l1.next
+    }
+    if (l2) {
+      sum += l2.val
+      l2 = l2.next
+    }
+    carry = sum > 9 ? 1 : 0
+    curr.next = new ListNode(sum % 10)
+    curr = curr.next
+  }
+  return temp.next
+}
+// @lc code=end
