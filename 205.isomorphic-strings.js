0c7ac8c3950f718467a8ceb1d914eb6fad6399c8
Matt <mwpryer@gmail.com>
2023-07-26 16:30:51 +0800

    Solve 205.isomorphic-strings.js

---
+++ 205.isomorphic-strings.js
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=205 lang=javascript
+ *
+ * [205] Isomorphic Strings
+ *
+ * https://leetcode.com/problems/isomorphic-strings/description/
+ *
+ * algorithms
+ * Easy (43.14%)
+ * Likes:    7215
+ * Dislikes: 1552
+ * Total Accepted:    941.7K
+ * Total Submissions: 2.2M
+ * Testcase Example:  '"egg"\n"add"'
+ *
+ * Given two strings s and t, determine if they are isomorphic.
+ *
+ * Two strings s and t are isomorphic if the characters in s can be replaced to
+ * get t.
+ *
+ * All occurrences of a character must be replaced with another character while
+ * preserving the order of characters. No two characters may map to the same
+ * character, but a character may map to itself.
+ *
+ *
+ * Example 1:
+ * Input: s = "egg", t = "add"
+ * Output: true
+ * Example 2:
+ * Input: s = "foo", t = "bar"
+ * Output: false
+ * Example 3:
+ * Input: s = "paper", t = "title"
+ * Output: true
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= s.length <= 5 * 10^4
+ * t.length == s.length
+ * s and t consist of any valid ascii character.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+  const map = {}
+  const paired = {}
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]] && map[s[i]] !== t[i]) return false
+    if (!map[s[i]] && paired[t[i]]) return false
+    map[s[i]] = t[i]
+    paired[t[i]] = true
+  }
+  return true
+}
+// @lc code=end
