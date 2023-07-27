99885a3a11d4e32dd606903b01460968f4e93d1b
Matt <mwpryer@gmail.com>
2023-07-28 02:47:31 +0800

    Solve 228.summary-ranges.js

---
+++ 228.summary-ranges.js
@@ -0,0 +1,94 @@
+/*
+ * @lc app=leetcode id=228 lang=javascript
+ *
+ * [228] Summary Ranges
+ *
+ * https://leetcode.com/problems/summary-ranges/description/
+ *
+ * algorithms
+ * Easy (49.17%)
+ * Likes:    3511
+ * Dislikes: 1835
+ * Total Accepted:    432.8K
+ * Total Submissions: 878.1K
+ * Testcase Example:  '[0,1,2,4,5,7]'
+ *
+ * You are given a sorted unique integer array nums.
+ *
+ * A range [a,b] is the set of all integers from a to b (inclusive).
+ *
+ * Return the smallest sorted list of ranges that cover all the numbers in the
+ * array exactly. That is, each element of nums is covered by exactly one of
+ * the ranges, and there is no integer x such that x is in one of the ranges
+ * but not in nums.
+ *
+ * Each range [a,b] in the list should be output as:
+ *
+ *
+ * "a->b" if a != b
+ * "a" if a == b
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [0,1,2,4,5,7]
+ * Output: ["0->2","4->5","7"]
+ * Explanation: The ranges are:
+ * [0,2] --> "0->2"
+ * [4,5] --> "4->5"
+ * [7,7] --> "7"
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [0,2,3,4,6,8,9]
+ * Output: ["0","2->4","6","8->9"]
+ * Explanation: The ranges are:
+ * [0,0] --> "0"
+ * [2,4] --> "2->4"
+ * [6,6] --> "6"
+ * [8,9] --> "8->9"
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 0 <= nums.length <= 20
+ * -2^31 <= nums[i] <= 2^31 - 1
+ * All the values of nums are unique.
+ * nums is sorted in ascending order.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function (nums) {
+  if (!nums.length) return []
+  const res = []
+  let first = nums[0]
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1] + 1) {
+      if (first === nums[i - 1]) {
+        res.push(first.toString())
+      } else {
+        res.push(first + "->" + nums[i - 1])
+      }
+      first = nums[i]
+    }
+  }
+  if (first === nums[nums.length - 1]) {
+    res.push(first.toString())
+  } else {
+    res.push(first + "->" + nums[nums.length - 1])
+  }
+  return res
+}
+// @lc code=end
