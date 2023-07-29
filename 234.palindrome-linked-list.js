f60f0aefa62c11bee737ec093b060944c4cef93f
Matt <mwpryer@gmail.com>
2023-07-30 04:08:59 +0800

    Solve 234.palindrome-linked-list.js

---
+++ 234.palindrome-linked-list.js
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=234 lang=javascript
+ *
+ * [234] Palindrome Linked List
+ *
+ * https://leetcode.com/problems/palindrome-linked-list/description/
+ *
+ * algorithms
+ * Easy (50.51%)
+ * Likes:    14713
+ * Dislikes: 802
+ * Total Accepted:    1.5M
+ * Total Submissions: 3M
+ * Testcase Example:  '[1,2,2,1]'
+ *
+ * Given the head of a singly linked list, return true if it is a palindrome or
+ * false otherwise.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [1,2,2,1]
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: head = [1,2]
+ * Output: false
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the list is in the range [1, 10^5].
+ * 0 <= Node.val <= 9
+ *
+ *
+ *
+ * Follow up: Could you do it in O(n) time and O(1) space?
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function (head) {
+  const vals = []
+  let curr = head
+  while (curr) {
+    vals.push(curr.val)
+    curr = curr.next
+  }
+  for (let i = 0; i < Math.floor(vals.length / 2); i++) {
+    if (vals[i] !== vals[vals.length - 1 - i]) return false
+  }
+  return true
+}
+// @lc code=end
