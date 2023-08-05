a225f74017281bae26abbed7bc2741fe90060281
Matt <mwpryer@gmail.com>
2023-08-05 21:39:21 +0100

    Solve 344.reverse-string.js

---
+++ 344.reverse-string.js
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=344 lang=javascript
+ *
+ * [344] Reverse String
+ *
+ * https://leetcode.com/problems/reverse-string/description/
+ *
+ * algorithms
+ * Easy (76.97%)
+ * Likes:    7649
+ * Dislikes: 1108
+ * Total Accepted:    2.2M
+ * Total Submissions: 2.8M
+ * Testcase Example:  '["h","e","l","l","o"]'
+ *
+ * Write a function that reverses a string. The input string is given as an
+ * array of characters s.
+ *
+ * You must do this by modifying the input array in-place with O(1) extra
+ * memory.
+ *
+ *
+ * Example 1:
+ * Input: s = ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ * Example 2:
+ * Input: s = ["H","a","n","n","a","h"]
+ * Output: ["h","a","n","n","a","H"]
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= s.length <= 10^5
+ * s[i] is a printable ascii character.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  for (let i = 0; i < Math.floor(s.length / 2); i++) {
+    const temp = s[i]
+    s[i] = s[s.length - 1 - i]
+    s[s.length - 1 - i] = temp
+  }
+}
+// @lc code=end
