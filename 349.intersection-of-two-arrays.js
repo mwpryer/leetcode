cafb2a35d8fa6f48dcc77e0724d2c9c0d0df1edc
Matt <mwpryer@gmail.com>
2023-08-06 16:09:09 +0100

    Solve 349.intersection-of-two-arrays.js

---
+++ 349.intersection-of-two-arrays.js
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=349 lang=javascript
+ *
+ * [349] Intersection of Two Arrays
+ *
+ * https://leetcode.com/problems/intersection-of-two-arrays/description/
+ *
+ * algorithms
+ * Easy (71.25%)
+ * Likes:    5044
+ * Dislikes: 2159
+ * Total Accepted:    893.1K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,2,2,1]\n[2,2]'
+ *
+ * Given two integer arrays nums1 and nums2, return an array of their
+ * intersection. Each element in the result must be unique and you may return
+ * the result in any order.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ * Output: [2]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * Output: [9,4]
+ * Explanation: [4,9] is also accepted.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= nums1.length, nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 1000
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function (nums1, nums2) {
+  const res = []
+  const dict = {}
+  for (const num of nums1) {
+    dict[num] = true
+  }
+  for (const num of nums2) {
+    if (dict[num]) {
+      res.push(num)
+      dict[num] = false
+    }
+  }
+  return res
+}
+// @lc code=end
