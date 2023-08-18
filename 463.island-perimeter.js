fa4b2eb73b9cbc81274ba82da54d8e61cead48b7
Matt <mwpryer@gmail.com>
2023-08-18 15:19:10 +0100

    Update 463.island-perimeter.js

--- 463.island-perimeter.js
+++ 463.island-perimeter.js
@@ -67,19 +67,17 @@
  * @return {number}
  */
 var islandPerimeter = function (grid) {
-  let perimeter = 0
+  let lands = 0
+  let overlaps = 0
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[0].length; j++) {
       if (grid[i][j] === 1) {
-        let edges = 4
-        if (i - 1 >= 0 && grid[i - 1][j] === 1) edges--
-        if (j + 1 < grid[0].length && grid[i][j + 1] === 1) edges--
-        if (i + 1 < grid.length && grid[i + 1][j] === 1) edges--
-        if (j - 1 >= 0 && grid[i][j - 1] === 1) edges--
-        perimeter += edges
+        lands++
+        if (i + 1 < grid.length && grid[i + 1][j] === 1) overlaps++
+        if (j + 1 < grid[0].length && grid[i][j + 1] === 1) overlaps++
       }
     }
   }
-  return perimeter
+  return 4 * lands - 2 * overlaps
 }
 // @lc code=end
