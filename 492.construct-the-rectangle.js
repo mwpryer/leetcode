7a686a1bd44da03ad2d9f1d4e18788b42a3a3c68
Matt <mwpryer@gmail.com>
2023-08-20 10:25:09 +0100

    Solve 492.construct-the-rectangle.js

---
+++ 492.construct-the-rectangle.js
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode id=492 lang=javascript
+ *
+ * [492] Construct the Rectangle
+ *
+ * https://leetcode.com/problems/construct-the-rectangle/description/
+ *
+ * algorithms
+ * Easy (55.61%)
+ * Likes:    626
+ * Dislikes: 371
+ * Total Accepted:    101.6K
+ * Total Submissions: 182.5K
+ * Testcase Example:  '4'
+ *
+ * A web developer needs to know how to design a web page's size. So, given a
+ * specific rectangular web page’s area, your job by now is to design a
+ * rectangular web page, whose length L and width W satisfy the following
+ * requirements:
+ *
+ *
+ * The area of the rectangular web page you designed must equal to the given
+ * target area.
+ * The width W should not be larger than the length L, which means L >= W.
+ * The difference between length L and width W should be as small as
+ * possible.
+ *
+ *
+ * Return an array [L, W] where L and W are the length and width of the web
+ * page you designed in sequence.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: area = 4
+ * Output: [2,2]
+ * Explanation: The target area is 4, and all the possible ways to construct it
+ * are [1,4], [2,2], [4,1].
+ * But according to requirement 2, [1,4] is illegal; according to requirement
+ * 3,  [4,1] is not optimal compared to [2,2]. So the length L is 2, and the
+ * width W is 2.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: area = 37
+ * Output: [37,1]
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: area = 122122
+ * Output: [427,286]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= area <= 10^7
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number} area
+ * @return {number[]}
+ */
+var constructRectangle = function (area) {
+  let length = area
+  let width = 1
+  for (let i = 0; i < area; i++) {
+    if (area % i === 0) {
+      if (area / i >= i && area / i - i < length - width) {
+        length = area / i
+        width = i
+      }
+    }
+  }
+  return [length, width]
+}
+// @lc code=end
