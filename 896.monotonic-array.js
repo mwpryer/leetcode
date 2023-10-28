3527ddf44ac15089c12d7912bcaa44f72c409bd8
Matt <mwpryer@gmail.com>
2023-10-28 14:57:45 +0100

    Solve 896.monotonic-array.js

---
+++ 896.monotonic-array.js
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode id=896 lang=javascript
+ *
+ * [896] Monotonic Array
+ *
+ * https://leetcode.com/problems/monotonic-array/description/
+ *
+ * algorithms
+ * Easy (61.08%)
+ * Likes:    2896
+ * Dislikes: 85
+ * Total Accepted:    388.9K
+ * Total Submissions: 636.7K
+ * Testcase Example:  '[1,2,2,3]'
+ *
+ * An array is monotonic if it is either monotone increasing or monotone
+ * decreasing.
+ *
+ * An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j].
+ * An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
+ *
+ * Given an integer array nums, return true if the given array is monotonic, or
+ * false otherwise.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [1,2,2,3]
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [6,5,4,4]
+ * Output: true
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: nums = [1,3,2]
+ * Output: false
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= nums.length <= 10^5
+ * -10^5 <= nums[i] <= 10^5
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = function (nums) {
+  let dir = 0
+  let prev = nums[0]
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] < prev) {
+      if (dir === -1) return false
+      dir = 1
+    } else if (nums[i] > prev) {
+      if (dir === 1) return false
+      dir = -1
+    }
+    prev = nums[i]
+  }
+  return true
+}
+// @lc code=end
