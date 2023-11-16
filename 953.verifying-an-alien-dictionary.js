8792c75aa09d6fc771d98f7aa3cbdb720dac0f51
Matt <mwpryer@gmail.com>
2023-11-16 22:01:26 +0000

    Solve 953.verifying-an-alien-dictionary.js

---
+++ 953.verifying-an-alien-dictionary.js
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode id=953 lang=javascript
+ *
+ * [953] Verifying an Alien Dictionary
+ *
+ * https://leetcode.com/problems/verifying-an-alien-dictionary/description/
+ *
+ * algorithms
+ * Easy (54.68%)
+ * Likes:    4776
+ * Dislikes: 1579
+ * Total Accepted:    479.7K
+ * Total Submissions: 877.2K
+ * Testcase Example:  '["hello","leetcode"]\n"hlabcdefgijkmnopqrstuvwxyz"'
+ *
+ * In an alien language, surprisingly, they also use English lowercase letters,
+ * but possibly in a different order. The order of the alphabet is some
+ * permutation of lowercase letters.
+ *
+ * Given a sequence of words written in the alien language, and the order of
+ * the alphabet, return true if and only if the given words are sorted
+ * lexicographically in this alien language.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+ * Output: true
+ * Explanation: As 'h' comes before 'l' in this language, then the sequence is
+ * sorted.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+ * Output: false
+ * Explanation: As 'd' comes after 'l' in this language, then words[0] >
+ * words[1], hence the sequence is unsorted.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+ * Output: false
+ * Explanation: The first three characters "app" match, and the second string
+ * is shorter (in size.) According to lexicographical rules "apple" > "app",
+ * because 'l' > '∅', where '∅' is defined as the blank character which is less
+ * than any other character (More info).
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= words.length <= 100
+ * 1 <= words[i].length <= 20
+ * order.length == 26
+ * All characters in words[i] and order are English lowercase letters.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} words
+ * @param {string} order
+ * @return {boolean}
+ */
+var isAlienSorted = function (words, order) {
+  const map = {}
+  for (let i = 0; i < order.length; i++) {
+    map[order[i]] = i
+  }
+
+  for (let i = 1; i < words.length; i++) {
+    const word1 = words[i - 1]
+    const word2 = words[i]
+    for (let j = 0; j < word1.length; j++) {
+      if (j >= word2.length) return false
+      if (word1[j] !== word2[j]) {
+        if (map[word1[j]] > map[word2[j]]) return false
+        break
+      }
+    }
+  }
+  return true
+}
+// @lc code=end
