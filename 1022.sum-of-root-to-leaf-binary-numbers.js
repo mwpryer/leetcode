f1b8aca034558c7ca7c77f14bbe2fb6c8cc4fc50
Matt <mwpryer@gmail.com>
2023-12-15 23:15:54 +0000

    Solve 1022.sum-of-root-to-leaf-binary-numbers.js

---
+++ 1022.sum-of-root-to-leaf-binary-numbers.js
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=1022 lang=javascript
+ *
+ * [1022] Sum of Root To Leaf Binary Numbers
+ *
+ * https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/description/
+ *
+ * algorithms
+ * Easy (73.30%)
+ * Likes:    3258
+ * Dislikes: 182
+ * Total Accepted:    201.3K
+ * Total Submissions: 274.6K
+ * Testcase Example:  '[1,0,1,0,1,0,1]'
+ *
+ * You are given the root of a binary tree where each node has a value 0 or 1.
+ * Each root-to-leaf path represents a binary number starting with the most
+ * significant bit.
+ *
+ *
+ * For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent
+ * 01101 in binary, which is 13.
+ *
+ *
+ * For all leaves in the tree, consider the numbers represented by the path
+ * from the root to that leaf. Return the sum of these numbers.
+ *
+ * The test cases are generated so that the answer fits in a 32-bits
+ * integer.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [1,0,1,0,1,0,1]
+ * Output: 22
+ * Explanation: (100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [0]
+ * Output: 0
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [1, 1000].
+ * Node.val is 0 or 1.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumRootToLeaf = function (root) {
+  let res = 0
+  let queue = [[root, 0]]
+  while (queue.length > 0) {
+    const n = queue.length
+    for (let i = 0; i < n; i++) {
+      const [curr, parentSum] = queue.shift()
+      const sum = parentSum * 2 + curr.val
+      if (!curr.left && !curr.right) res += sum
+      if (curr.left) queue.push([curr.left, sum])
+      if (curr.right) queue.push([curr.right, sum])
+    }
+  }
+  return res
+}
+// @lc code=end
