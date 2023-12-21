5eeb1a06f710d00c968f6d296ace7185bc915579
Matt <mwpryer@gmail.com>
2023-12-21 23:31:49 +0000

    Update 1089.duplicate-zeros.js

--- 1089.duplicate-zeros.js
+++ 1089.duplicate-zeros.js
@@ -56,17 +56,16 @@
  */
 var duplicateZeros = function (arr) {
   let n = arr.length
-  let i = 0
-  while (i < n) {
+  let count = 0
+  for (let i = 0; i < n; i++) {
+    if (arr[i] === 0) count++
+  }
+  for (let i = n - 1; i >= 0; i--) {
+    if (i + count < n) arr[i + count] = arr[i]
     if (arr[i] === 0) {
-      for (let j = n - 1; j > i; j--) {
-        arr[j] = arr[j - 1]
-      }
-      i += 2
-    } else {
-      i++
+      count--
+      if (i + count < n) arr[i + count] = arr[i]
     }
   }
-  arr.length = n
 }
 // @lc code=end
