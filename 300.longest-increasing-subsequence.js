3e1906fefc1993f8e04de55c7e42349a2565417b
Matt <mwpryer@gmail.com>
2024-01-05 19:43:01 +0800

    Solve 300.longest-increasing-subsequence.js

---
+++ 300.longest-increasing-subsequence.js
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=300 lang=javascript
+ *
+ * [300] Longest Increasing Subsequence
+ *
+ * https://leetcode.com/problems/longest-increasing-subsequence/description/
+ *
+ * algorithms
+ * Medium (52.65%)
+ * Likes:    18332
+ * Dislikes: 345
+ * Total Accepted:    1.3M
+ * Total Submissions: 2.4M
+ * Testcase Example:  '[10,9,2,5,3,7,101,18]'
+ *
+ * Given an integer array nums, return the length of the longest strictly
+ * increasing subsequence.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [10,9,2,5,3,7,101,18]
+ * Output: 4
+ * Explanation: The longest increasing subsequence is [2,3,7,101], therefore
+ * the length is 4.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [0,1,0,3,2,3]
+ * Output: 4
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: nums = [7,7,7,7,7,7,7]
+ * Output: 1
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= nums.length <= 2500
+ * -10^4 <= nums[i] <= 10^4
+ *
+ *
+ *
+ * Follow up: Can you come up with an algorithm that runs in O(n log(n)) time
+ * complexity?
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+  const lengths = new Array(nums.length).fill(1)
+  let max = 1
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) {
+        lengths[i] = Math.max(lengths[i], lengths[j] + 1)
+        max = Math.max(max, lengths[i])
+      }
+    }
+  }
+  return max
+}
+// @lc code=end
