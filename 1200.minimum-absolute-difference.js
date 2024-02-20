ce435e58814328a2e4ace79eb7ac549bc0782bb8
Matt <mwpryer@gmail.com>
2024-02-20 23:27:36 +0800

    Solve 1200.minimum-absolute-difference.js

---
+++ 1200.minimum-absolute-difference.js
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode id=1200 lang=javascript
+ *
+ * [1200] Minimum Absolute Difference
+ *
+ * https://leetcode.com/problems/minimum-absolute-difference/description/
+ *
+ * algorithms
+ * Easy (69.43%)
+ * Likes:    2308
+ * Dislikes: 74
+ * Total Accepted:    182.6K
+ * Total Submissions: 263K
+ * Testcase Example:  '[4,2,1,3]'
+ *
+ * Given an array of distinct integers arr, find all pairs of elements with the
+ * minimum absolute difference of any two elements.
+ *
+ * Return a list of pairs in ascending order(with respect to pairs), each pair
+ * [a, b] follows
+ *
+ *
+ * a, b are from arr
+ * a < b
+ * b - a equals to the minimum absolute difference of any two elements in
+ * arr
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: arr = [4,2,1,3]
+ * Output: [[1,2],[2,3],[3,4]]
+ * Explanation: The minimum absolute difference is 1. List all pairs with
+ * difference equal to 1 in ascending order.
+ *
+ * Example 2:
+ *
+ *
+ * Input: arr = [1,3,6,10,15]
+ * Output: [[1,3]]
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: arr = [3,8,-10,23,19,-4,-14,27]
+ * Output: [[-14,-10],[19,23],[23,27]]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 2 <= arr.length <= 10^5
+ * -10^6 <= arr[i] <= 10^6
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @return {number[][]}
+ */
+var minimumAbsDifference = function (arr) {
+  arr.sort((a, b) => a - b)
+  let res = []
+  let min = Math.abs(arr[1] - arr[0])
+  for (let i = 0; i < arr.length - 1; i++) {
+    const diff = Math.abs(arr[i + 1] - arr[i])
+    if (diff < min) {
+      res = [[arr[i], arr[i + 1]]]
+      min = diff
+    } else if (diff === min) {
+      res.push([arr[i], arr[i + 1]])
+    }
+  }
+  return res
+}
+// @lc code=end
